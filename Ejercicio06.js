ad96d3d838c5e1c3f591e0c6a32b34efef34146e
RojasDulce <ti18110102@itess.edu.mx>
2022-04-25 19:29:35 -0500

    Tablas de multiplicar con for en JavaScript

---
+++ Ejercicio06.js
@@ -0,0 +1,8 @@
+var tablaDe = 2;
+
+for (tablaDe = 2; tablaDe <= 12; tablaDe++) {
+  for (let numero = 1; numero <= 10; numero++) {
+    console.log(tablaDe + "*" + numero + "=" + tablaDe * numero);
+  }
+  console.log("      ");
+}
